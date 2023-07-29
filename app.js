0415ffe344c2d1caf2b000f4da5d3ef65ec38b52
GustavoEAybar <aybargustavoe@gmail.com>
2023-07-29 15:31:57 -0300

    set up .gitignore and start of practices

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log("Hola mundo!");
+console.info("Hola mundo!");
+console.warn("Hola mundo de nuevo!");
+console.error("Hola mundo de nuevo 2!");
---
+++ demo.js
@@ -0,0 +1,6 @@
+console.log("hola");
+console.error("hola de nuevo!");
+console.warn("hola de nuevo 2!");
+console.log([1, 2, 3, 4]);
+console.table([1, 2, 3, 4]);
+
