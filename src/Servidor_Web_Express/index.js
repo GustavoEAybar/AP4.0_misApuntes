fd133e15b80b53acf96fb0bbc028c189f0a8e89d
GustavoEAybar <aybargustavoe@gmail.com>
2023-08-10 15:42:50 -0300

    end of class practice 4

--- src/Servidor_Web_Express/index.js
+++ src/Servidor_Web_Express/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const PORT = 3050;
 const app = express();
+
 app.get("/", (req,res) => {
-    res.send("Hola mundo! Hola Express!");
+    res.status(200).send("<h1>Bienvenidos a nuestra WEB</h1>");
 });
 
-app.listen(3000, () => {
-    console.log(`Servidor iniciado en el puerto 3000`);
+app.get("/cursos", (req,res) => {
+    res.status(200).send("<h1>Bienvenidos a nuestros cursos</h1>");
+});
+
+app.get("/contactos", (req,res) => {
+    res.status(200).send("<h1>Bienvenidos a nuestros contactos</h1>");
+});
+
+// app.get("*", (req,res) => {
+//     res.status(404).send("<h1>Error: 404 - No se encuentra la ruta o recurso solicitado</h1>");
+// });
+
+// app.use((req,res) => {
+//     res.status(404).send(`{"Error": "404", "description": "No se encuentra la ruta o recurso solicitado"}`);
+// });
+
+app.use((req,res) => {
+    res.status(404).json(`{"Error": "404", "description": "No se encuentra la ruta o recurso solicitado"}`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en el puerto ${PORT}`);
 })
