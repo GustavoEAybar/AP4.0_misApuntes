83117ed39e22ca35844897a8823f10135879c6ec
GustavoEAybar <aybargustavoe@gmail.com>
2023-08-10 20:13:00 -0300

    end class 06 template engine

--- src/Motor_De_Plantillas/index.js
+++ src/Motor_De_Plantillas/index.js
@@ -1,17 +1,32 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 const PORT = 3000;
+
+const computerProducts = [
+    {name:'Notebook Lenobo', precio: 50000},
+    {name: 'Notebook Asus',precio: 30000},
+    {name: 'Notebook HP',precio: 20000}
+];
 
 app.set('view engine', 'ejs');
 app.use(express.static('views'));
 
 app.get('/', (req, res) => {
-    res.send(200);
+    const data = {
+        title: 'Mi sitio web con EJS',
+        message: 'Bienvenido a mi sitio web generado a partir de un morot de plantillas.'
+    };
+    res.render('index', data);
 });
 
 app.get('/productos', (req, res) => {
-    res.send(200);
+    const data = {
+        title: 'Mi sitio web con EJS',
+        message: 'Bienvenido a mi sitio web generado a partir de un morot de plantillas.',
+        products: computerProducts
+    };
+    res.render('products', data);
 });
 
 app.listen(PORT, () => {
