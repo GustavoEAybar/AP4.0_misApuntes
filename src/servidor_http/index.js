5165d2bd1f4ac09c101f772788c56a6fa78470cd
GustavoEAybar <aybargustavoe@gmail.com>
2023-08-10 14:42:29 -0300

    terminate server with node and initialize one with express

--- src/servidor_http/index.js
+++ src/servidor_http/index.js
@@ -1,9 +1,30 @@
 const http = require ("http");
-const PORT = 3000;
-const server = http.createServer((request, response) => {
-    response.statusCode = 200;
-    response.setHeader("Content-Type", "text/plain");
-    response.end(`Hola mundo!`)
+
+const PORT = 3008;
+
+const server = http.createServer((req, res) => {
+    // response.statusCode = 200;
+    // response.setHeader("Content-Type", "text/plain");
+    // response.end(`Hola mundo!`)
+
+    switch (req.url){
+        case "/":
+            res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+            res.end(`<h1>Biemvenidos a Nuestra WEB</h1>`);
+            break;
+        case "/cursos":
+            res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+            res.end(`<h1>Biemvenidos a nuestra sección de cursos</h1>`);
+            break;
+        case "/contactos":
+            res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+            res.end(`<h1>Biemvenidos a nuestra seccion de contacto</h1>`);
+            break;
+        default:
+            res.writeHead(404, {"Content-Type": "text/html; charset=utf-8"});
+            res.end(`<h1>Ruta Inexistente</h1>`);
+            break;
+    }
 });
 
 server.listen (PORT, () => {
