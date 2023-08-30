7e83337c207d4a50942e1638d88478e46935c416
GustavoEAybar <aybargustavoe@gmail.com>
2023-08-30 14:59:15 -0300

    resolve errors

--- 07.fileSystem/index.js
+++ 07.fileSystem/index.js
@@ -1,44 +1,56 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 const PORT = 3000;
-const fs = require ("fs");
+const fs = require("fs");
 
 app.get("/productos", (req, res) => {
-  fs.readFile("products.json","utf-8",(error, data) => {
+  fs.readFile("products.json", "utf-8", (error, data) => {
     if (error) {
-      res.json({ error: error})
-      return
+      res.json({ error: error });
+      return;
     }
-    res.json(JSON.parse(data))
-  })
-})
+    res.json(JSON.parse(data));
+  });
+});
 
 app.get("/productos/id/:id", (req, res) => {
-  let idProductoConsultado = parseInt(req.params.id)
-  fs.readFile("products.json","utf-8",(error, data) => {
-    if(error) {
-      res.json({ error: error })
-      return
+  let idProductoConsultado = parseInt(req.params.id);
+  fs.readFile("products.json", "utf-8", (error, data) => {
+    if (error) {
+      res.json({ error: error });
+      return;
     }
-    let productos =JSON.parse(data)
-    let respuesta = prodctos.find((producto) => producto.id === idProductoConsultado)
-    respuesta ? res.json(respuesta) : res.json({error: `No existen conincidencias para el id ${idProductoConsultado}`})
-  })
-})
+    let productos = JSON.parse(data);
+    let respuesta = productos.find(
+      (producto) => producto.id === idProductoConsultado
+    );
+    respuesta
+      ? res.json(respuesta)
+      : res.json({
+          error: `No existen conincidencias para el id ${idProductoConsultado}`,
+        });
+  });
+});
 
-app.get("/productos/nombre/:nombre", (req,res)=>{
-  let nombreProductoConsultado=req.params.nombre.toLowerCase()
-  fs.readFile("products.json","utf-8", (error, data) => {
-    if(error){
-      res.json({error: error})
-      return
+app.get("/productos/nombre/:nombre", (req, res) => {
+  let nombreProductoConsultado = req.params.nombre.toLowerCase();
+  fs.readFile("products.json", "utf-8", (error, data) => {
+    if (error) {
+      res.json({ error: error });
+      return;
     }
-    let productos =JSON.parse(data);
-    let respuesta =productos.filter(elemento => elemento.nombre.toLowerCase().Includes(nombreProductoConsultado)); 
-    respuesta.length ? res.json(respuesta) : res.json({error: `No existen conincidencias para el producto ${nombreProductoConsultado}`})
-  })
-})
+    let productos = JSON.parse(data);
+    let respuesta = productos.filter(elemento =>
+      elemento.nombre.toLowerCase().includes(nombreProductoConsultado)
+    );
+    respuesta.length
+      ? res.json(respuesta)
+      : res.json({
+          error: `No existen conincidencias para el producto ${nombreProductoConsultado}`,
+        });
+  });
+});
 
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`)
-})
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
